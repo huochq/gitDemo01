095323773c339a2af628c7ef5663e619dec58692
huochq <huochq@neusoft.com>
2022-07-05 21:17:45 +0800

    第三次修改

--- src/com/neusoft/Main.java
+++ src/com/neusoft/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("第二次修改");
+        System.out.println("第三次修改");
     }
 }
